0b71141535d6f1f01f6bdff414b6e686d485b70e
TalhaHabib <71073238+talhaHabib-sys@users.noreply.github.com>
2021-10-16 21:12:41 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -14,6 +14,17 @@ function App() {
     aup(arr)
     
   }
+  function edt(e)
+  {
+    document.getElementById('inp').value=e
+  }
+  function Empty(){
+    
+    let arr=[...aset]
+    arr=[]
+    aup(arr)
+    
+  }
   console.log(aset)
   return (
     <div className="Main">
@@ -24,13 +35,14 @@ function App() {
       <input type='text' onChange={(e)=>up(e.target.value)} placeholder='AddItem' id='inp'/>
       <button onClick={addItem} >+</button>
       <button onClick={RemoveItem} id='abc'>-</button>
+      <button onClick={Empty}>Empty list</button>
 </div>
 
 
 <body/>
       <div style={{top:'10px'}}>
       <ul style={{backgroundColor:'lightgreen'}}> 
-        {aset.map((e)=>{return <li style={{color:'smokewhite' ,borderBottom:'1px solid gray',borderTop:'1px solid gray'}}>{e}</li>})}
+        {aset.map((e,i)=>{return <li style={{color:'smokewhite' ,borderBottom:'1px solid gray',borderTop:'1px solid gray'}} id='ab'>{e}<button>Edit</button></li>})}
           </ul>
           </div>
       
